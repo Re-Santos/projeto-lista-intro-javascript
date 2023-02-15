2f2dfebb1577cf134a20efe63fb215001016b783
Re-Santos <renata.prog@gmail.com>
2023-02-15 19:22:50 -0300

    salvando alterações

--- exercicios.js
+++ exercicios.js
@@ -46,8 +46,7 @@ function calculaIMC(peso, altura) {
   return imc
     
 }
-*/
-
+*//*
 // EXERCÍCIO 04
 function imprimeInformacoesUsuario() {
   // implemente sua lógica aqui
@@ -59,9 +58,7 @@ function imprimeInformacoesUsuario() {
 
   console.log (mensagem)
 }
-
-
-
+*/
 /*
 
 // EXERCÍCIO 05
@@ -74,7 +71,7 @@ function imprimeTresCoresFavoritas() {
 
   console.log (minhasCoresFavoritas)
 }
-/*
+
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
   // implemente sua lógica aqui
@@ -82,13 +79,13 @@ function retornaStringEmMaiuscula(string) {
   return frase
 
 }
-/*
+*//*
 // EXERCÍCIO 07
 function calculaIngressosEspetaculo(custo, valorIngresso) {
   // implemente sua lógica aqui
-  const precoCusto= Number custo
-  const precoIngresso= Number valorIngresso
-  const precisaVender= Number precoCusto/precoIngresso
+  const precoCusto= custo
+  const precoIngresso= valorIngresso
+  const precisaVender= precoCusto/precoIngresso
 
   return precisaVender
  
@@ -104,7 +101,7 @@ function checaStringsMesmoTamanho(string1, string2) {
   const mesmoTamanho= tamanhoFrase1 === tamanhoFrase2 
   return mesmoTamanho
   
-}*/
+}*//*
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
@@ -114,43 +111,70 @@ function retornaPrimeiroElemento(array) {
   return primeiroElemento
 
 }
-
+*/
 
 
 // EXERCÍCIO 10
 function retornaUltimoElemento(array) {
   // implemente sua lógica aqui
   
-  return array[array.length - 1];
-
+    return array[array.length - 1]
+  
 }
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
   // implemente sua lógica aqui
-
+  const primeiro = array[0]
+  const ultimo = array[array.length - 1]
+  array[0] = ultimo
+  array[array.length - 1] = primeiro
+  return array
+ 
 }
-
+/*
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
   // implemente sua lógica aqui
+  const frase1= string1.toLowerCase()
+  const frase2= string2.toLowerCase()
+  const check= frase1===frase2
+  return check
+
 
 }
-
+/*
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
   // implemente sua lógica aqui
-
+  const anoAtual= prompt(`Qual o ano atual?`)
+  const anoNascimento= prompt(`Qual o ano de nascimento`)
+  const anoEmissao= prompt(`Em que ano a carteira de identidade foi emitida?`)
+  const renovacao5Anos= anoAtual-anoNascimento>20&& anoAtual-anoEmissao>=5
+  const renovacao10Anos=anoAtual-anoNascimento>20 && anoAtual-anoNascimento<50 && anoAtual-anoEmissao>=10
+  const renovacao15anos= anoAtual-anoNascimento>50&& anoAtual-anoEmissao>=15
+  const checkRenovacao= (renovacao5Anos(true),renovacao10Anos(true),renovacao15anos(true))
+  console.log (checkRenovacao)
+*//*
 }
-
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
   // implemente sua lógica aqui
+  const bissextos = (ano % 400 === 0)
+  const bissextos2= (ano % 4 === 0 && ano % 100 !== 0)
+  const naoBissextos = (ano % 4 === 0 && ano % 100 !== 0 || ano % 400 === 0)
 
-}
+}*/
 
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
   // implemente sua lógica aqui
+  const temMaisDezoito = Boolean (prompt(`Você tem mais de 18 anos?`))
+  const ensinoMedio = Boolean (prompt(`Você possui o ensino médio completo?`))
+  const disponibilidade = Boolean (prompt(`Você possui disponibilidade exclusiva durante os horários do curso?`))
+ 
+  const checkValidade = temMaisDezoito === ensinoMedio === disponibilidade
 
-}*/
+  console.log (checkValidade)
+
+}
